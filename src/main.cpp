cc5300741bf7f9bbc8425a578c464e6a7b8ae78a
Lóránt Pintér <lorant.pinter@gmail.com>
2023-12-04 16:11:23 +0100

    Connect to WIFI

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 
 #include <WiFi.h>
-#include <WiFiMulti.h>
 #include <WiFiManager.h>
+#include <WiFiMulti.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -33,12 +33,32 @@ void blinkLED2(void* parameter) {
     }
 }
 
+WiFiManager wifiManager;
+
+void connectToWiFiTask(void* parameter) {
+    // Explicitly set mode, ESP defaults to STA+AP
+    WiFi.mode(WIFI_STA);
+
+    wifiManager.autoConnect("AutoConnectAP");
+
+    vTaskDelete(NULL);
+}
+
 void setup() {
     Serial.begin(115200);
     Serial.println("Starting up...");
-}
 
-void loop() {
+    // TODO What's a good stack size here?
+    xTaskCreate(connectToWiFiTask, "Connect to WiFi", 10000, NULL, 1, NULL);
+
+    // TODO Use time server from MDNS
+    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+    WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info) {
+        Serial.println("WiFi: connected to " + WiFi.SSID());
+    },
+        ARDUINO_EVENT_WIFI_STA_CONNECTED);
+
     // Initialize the LED pin as an output
     pinMode(ledPin1, OUTPUT);
     pinMode(ledPin2, OUTPUT);
@@ -46,4 +66,11 @@ void loop() {
     // Now create the tasks
     xTaskCreate(blinkLED1, "LED1 Blink", 1000, NULL, 1, NULL);
     xTaskCreate(blinkLED2, "LED2 Blink", 1000, NULL, 1, NULL);
+}
+
+void loop() {
+    Serial.print("\033[1G\033[0K");
+    Serial.print("Uptime: \033[33m" + String(millis()) + "\033[0m ms");
+    Serial.print(", wifi connected: \033[33m" + String(WiFi.isConnected()) + "\033[0m ms");
+    delay(100);
 }
