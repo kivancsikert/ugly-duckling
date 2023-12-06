a083a468780ecb1210fadf6aea0e692385748012
Lóránt Pintér <lorant.pinter@gmail.com>
2023-12-06 14:30:53 +0100

    Emit WIFI ready when we got IP, not when WIFI is connected

--- src/kernel/drivers/WiFiDriver.hpp
+++ src/kernel/drivers/WiFiDriver.hpp
@@ -22,13 +22,30 @@ protected:
         // Explicitly set mode, ESP defaults to STA+AP
         WiFi.mode(WIFI_STA);
 
-        // TODO Should we clear the event bit when disconnected?
         WiFi.onEvent(
-            [this](WiFiEvent_t event, WiFiEventInfo_t info) {
-                Serial.println("WiFi: connected");
-                emitEventFromISR();
+            [](WiFiEvent_t event, WiFiEventInfo_t info) {
+                Serial.println("WiFi: connected to " + String(info.wifi_sta_connected.ssid, info.wifi_sta_connected.ssid_len));
             },
             ARDUINO_EVENT_WIFI_STA_CONNECTED);
+        WiFi.onEvent(
+            [&](WiFiEvent_t event, WiFiEventInfo_t info) {
+                Serial.println("WiFi: got IP " + IPAddress(info.got_ip.ip_info.ip.addr).toString()
+                    + ", netmask: " + IPAddress(info.got_ip.ip_info.netmask.addr).toString()
+                    + ", gateway: " + IPAddress(info.got_ip.ip_info.gw.addr).toString());
+                emitEventFromISR();
+            },
+            ARDUINO_EVENT_WIFI_STA_GOT_IP);
+        WiFi.onEvent(
+            [](WiFiEvent_t event, WiFiEventInfo_t info) {
+                Serial.println("WiFi: lost IP address");
+            },
+            ARDUINO_EVENT_WIFI_STA_LOST_IP);
+        WiFi.onEvent(
+            [](WiFiEvent_t event, WiFiEventInfo_t info) {
+                Serial.println("WiFi: disconnected from " + String(info.wifi_sta_disconnected.ssid, info.wifi_sta_disconnected.ssid_len));
+                // TODO Should we clear the event bit when disconnected?
+            },
+            ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
 
         wifiManager.autoConnect("AutoConnectAP");
     }
