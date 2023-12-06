6921152a27f7efc35321bf847daaa740a9f335a4
Lóránt Pintér <lorant.pinter@gmail.com>
2023-12-06 23:57:48 +0100

    Convert more to use FTask

--- src/kernel/drivers/OtaDriver.hpp
+++ src/kernel/drivers/OtaDriver.hpp
@@ -5,69 +5,60 @@
 
 #include <kernel/drivers/WiFiDriver.hpp>
 
-#include <kernel/Task.hpp>
+#include <kernel/FTask.hpp>
 
 namespace farmhub { namespace kernel { namespace drivers {
 
-class OtaDriver
-    : public IntermittentLoopTask {
+class OtaDriver {
 
 public:
-    OtaDriver(Event& networkReady, const String& hostname)
-        : IntermittentLoopTask("OTA")
-        , networkReady(networkReady)
-        , hostname(hostname) {
-    }
+    OtaDriver(Event& networkReady, const String& hostname) {
+        FTask::runTask("OTA", [&](FTask& task) {
+            networkReady.await();
 
-protected:
-    void setup() override {
-        networkReady.await();
+            ArduinoOTA.setHostname(hostname.c_str());
 
-        ArduinoOTA.setHostname(hostname.c_str());
-        ArduinoOTA.onStart([&]() {
-            Serial.println("Start");
-            updating = true;
-        });
-        ArduinoOTA.onEnd([&]() {
-            Serial.println("\nEnd");
-            updating = false;
-        });
-        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-        });
-        ArduinoOTA.onError([&](ota_error_t error) {
-            Serial.printf("Web socket error[%u]\n", error);
-            if (error == OTA_AUTH_ERROR) {
-                Serial.println("Auth Failed");
-            } else if (error == OTA_BEGIN_ERROR) {
-                Serial.println("Begin Failed");
-            } else if (error == OTA_CONNECT_ERROR) {
-                Serial.println("Connect Failed");
-            } else if (error == OTA_RECEIVE_ERROR) {
-                Serial.println("Receive Failed");
-            } else if (error == OTA_END_ERROR) {
-                Serial.println("End Failed");
-            } else {
-                Serial.println("Other error");
+            bool updating = false;
+            ArduinoOTA.onStart([&]() {
+                Serial.println("Start");
+                updating = true;
+            });
+            ArduinoOTA.onEnd([&]() {
+                Serial.println("\nEnd");
+                updating = false;
+            });
+            ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+                Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+            });
+            ArduinoOTA.onError([&](ota_error_t error) {
+                Serial.printf("Web socket error[%u]\n", error);
+                if (error == OTA_AUTH_ERROR) {
+                    Serial.println("Auth Failed");
+                } else if (error == OTA_BEGIN_ERROR) {
+                    Serial.println("Begin Failed");
+                } else if (error == OTA_CONNECT_ERROR) {
+                    Serial.println("Connect Failed");
+                } else if (error == OTA_RECEIVE_ERROR) {
+                    Serial.println("Receive Failed");
+                } else if (error == OTA_END_ERROR) {
+                    Serial.println("End Failed");
+                } else {
+                    Serial.println("Other error");
+                }
+                updating = false;
+            });
+            ArduinoOTA.begin();
+
+            Serial.println("OTA initialized on hostname " + hostname);
+
+            while (true) {
+                ArduinoOTA.handle();
+                return updating
+                    ? seconds::zero()
+                    : seconds(1);
             }
-            updating = false;
         });
-        ArduinoOTA.begin();
-
-        Serial.println("OTA initialized on hostname " + hostname);
     }
-
-    milliseconds loopAndDelay() override {
-        ArduinoOTA.handle();
-        return updating
-            ? seconds::zero()
-            : seconds(1);
-    }
-
-private:
-    Event& networkReady;
-    const String hostname;
-    bool updating = false;
 };
 
 }}}    // namespace farmhub::kernel::drivers
