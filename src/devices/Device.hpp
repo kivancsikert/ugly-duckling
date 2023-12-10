98ba9f3c698178b8f33ff1bd98abf6ec505190e7
Lóránt Pintér <lorant.pinter@gmail.com>
2023-12-10 20:35:18 +0100

    Move console printer to device

--- src/devices/Device.hpp
+++ src/devices/Device.hpp
@@ -2,12 +2,76 @@
 
 #include <kernel/Application.hpp>
 #include <kernel/PwmManager.hpp>
+#include <kernel/drivers/BatteryDriver.hpp>
 #include <kernel/drivers/LedDriver.hpp>
 
 using namespace farmhub::kernel;
 using namespace farmhub::kernel::drivers;
 
 namespace farmhub { namespace devices {
+
+class ConsolePrinter {
+public:
+    ConsolePrinter() {
+        static const String spinner = "|/-\\";
+        Task::loop("ConsolePrinter", 32 * 1024, 1, [this](Task& task) {
+            Serial.print("\033[1G\033[0K");
+
+            counter = (counter + 1) % spinner.length();
+            Serial.print("[" + spinner.substring(counter, counter + 1) + "] ");
+
+            Serial.print("\033[33m" + wifiStatus() + "\033[0m");
+            Serial.print(", IP: \033[33m" + WiFi.localIP().toString() + "\033[0m");
+
+            Serial.print(", uptime: \033[33m" + String(millis()) + "\033[0m ms");
+            time_t now;
+            struct tm timeinfo;
+            time(&now);
+            localtime_r(&now, &timeinfo);
+            Serial.print(&timeinfo, ", UTC: \033[33m%Y-%m-%d %H:%M:%S\033[0m");
+
+            BatteryDriver* battery = this->battery.load();
+            if (battery != nullptr) {
+                Serial.printf(", battery: \033[33m%.2f V\033[0m", battery->getVoltage());
+            }
+
+            Serial.print(" ");
+            Serial.flush();
+            task.delayUntil(milliseconds(100));
+        });
+    }
+
+    void registerBattery(BatteryDriver& battery) {
+        this->battery = &battery;
+    }
+
+private:
+    static String wifiStatus() {
+        switch (WiFi.status()) {
+            case WL_NO_SHIELD:
+                return "NO SHIELD";
+            case WL_IDLE_STATUS:
+                return "IDLE STATUS";
+            case WL_NO_SSID_AVAIL:
+                return "NO SSID AVAIL";
+            case WL_SCAN_COMPLETED:
+                return "SCAN COMPLETED";
+            case WL_CONNECTED:
+                return "CONNECTED";
+            case WL_CONNECT_FAILED:
+                return "CONNECT FAILED";
+            case WL_CONNECTION_LOST:
+                return "CONNECTION LOST";
+            case WL_DISCONNECTED:
+                return "DISCONNECTED";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    int counter;
+    std::atomic<BatteryDriver*> battery { nullptr };
+};
 
 class ConsoleProvider {
 public:
@@ -24,6 +88,11 @@ public:
         : statusLed("status", statusPin) {
     }
 
+protected:
+#ifdef FARMHUB_DEBUG
+    ConsolePrinter consolePrinter;
+#endif
+
 public:
     LedDriver statusLed;
     Application<TDeviceConfiguration> application { statusLed };
@@ -36,6 +105,9 @@ public:
     BatteryPoweredDevice(gpio_num_t statusPin, gpio_num_t batteryPin, float batteryVoltageDividerRatio)
         : Device<TDeviceConfiguration>(statusPin)
         , batteryDriver(batteryPin, batteryVoltageDividerRatio) {
+#ifdef FARMHUB_DEBUG
+        Device<TDeviceConfiguration>::consolePrinter.registerBattery(batteryDriver);
+#endif
         Device<TDeviceConfiguration>::application.registerTelemetryProvider("battery", batteryDriver);
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <atomic>
 #include <chrono>
 
 #include <Arduino.h>
@@ -24,72 +25,13 @@ using namespace farmhub::kernel::drivers;
 
 using namespace farmhub::devices;
 
-class ConsolePrinter {
-public:
-    ConsolePrinter(BatteryDriver& batteryDriver) {
-        static const String spinner = "|/-\\";
-        Task::loop("ConsolePrinter", 32 * 1024, 1, [this, &batteryDriver](Task& task) {
-            Serial.print("\033[1G\033[0K");
-
-            counter = (counter + 1) % spinner.length();
-            Serial.print("[" + spinner.substring(counter, counter + 1) + "] ");
-
-            Serial.print("\033[33m" + wifiStatus() + "\033[0m");
-            Serial.print(", IP: \033[33m" + WiFi.localIP().toString() + "\033[0m");
-
-            Serial.print(", uptime: \033[33m" + String(millis()) + "\033[0m ms");
-            time_t now;
-            struct tm timeinfo;
-            time(&now);
-            localtime_r(&now, &timeinfo);
-            Serial.print(&timeinfo, ", UTC: \033[33m%Y-%m-%d %H:%M:%S\033[0m");
-
-            Serial.printf(", battery: \033[33m%.2f V\033[0m", batteryDriver.getVoltage());
-
-            Serial.print(" ");
-            Serial.flush();
-            task.delayUntil(milliseconds(100));
-        });
-    }
-
-private:
-    static String wifiStatus() {
-        switch (WiFi.status()) {
-            case WL_NO_SHIELD:
-                return "NO SHIELD";
-            case WL_IDLE_STATUS:
-                return "IDLE STATUS";
-            case WL_NO_SSID_AVAIL:
-                return "NO SSID AVAIL";
-            case WL_SCAN_COMPLETED:
-                return "SCAN COMPLETED";
-            case WL_CONNECTED:
-                return "CONNECTED";
-            case WL_CONNECT_FAILED:
-                return "CONNECT FAILED";
-            case WL_CONNECTION_LOST:
-                return "CONNECTION LOST";
-            case WL_DISCONNECTED:
-                return "DISCONNECTED";
-            default:
-                return "UNKNOWN";
-        }
-    }
-
-    int counter;
-};
-
 class Main {
 #if defined(MK4)
     UglyDucklingMk4 device;
-    // TODO Add console printer for MK4, without battery driver
-    // ConsolePrinter consolePrinter { device.batteryDriver };
 #elif defined(MK5)
     UglyDucklingMk5 device;
-    ConsolePrinter consolePrinter { device.batteryDriver };
 #elif defined(MK6)
     UglyDucklingMk6 device;
-    ConsolePrinter consolePrinter { device.batteryDriver };
 #endif
 
 #if defined(MK5) || defined(MK6)
