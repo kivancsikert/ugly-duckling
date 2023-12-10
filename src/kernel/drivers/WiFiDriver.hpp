f451a296a74b50a04f7ed03aacb2ca61db6618f7
Lóránt Pintér <lorant.pinter@gmail.com>
2023-12-10 22:11:52 +0100

    Only create WiFiManager when needed

--- src/kernel/drivers/WiFiDriver.hpp
+++ src/kernel/drivers/WiFiDriver.hpp
@@ -16,16 +16,6 @@ public:
     WiFiDriver(StateSource& networkReady, StateSource& configPortalRunning, const String& hostname) {
         WiFi.mode(WIFI_STA);
         WiFi.setHostname(hostname.c_str());
-        wifiManager.setHostname(hostname.c_str());
-        wifiManager.setConfigPortalTimeout(180);
-        wifiManager.setAPCallback([this, &configPortalRunning](WiFiManager* wifiManager) {
-            Serial.println("WiFi: entered config portal");
-            configPortalRunning.setFromISR();
-        });
-        wifiManager.setConfigPortalTimeoutCallback([this, &configPortalRunning]() {
-            Serial.println("WiFi: config portal timed out");
-            configPortalRunning.clearFromISR();
-        });
 
         WiFi.onEvent(
             [](WiFiEvent_t event, WiFiEventInfo_t info) {
@@ -56,8 +46,19 @@ public:
             },
             ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
 
-        Task::run("WiFi", 4096, [this, &networkReady, hostname](Task& task) {
+        Task::run("WiFi", 4096, [this, &networkReady, &configPortalRunning, hostname](Task& task) {
             while (true) {
+                WiFiManager wifiManager;
+                wifiManager.setHostname(hostname.c_str());
+                wifiManager.setConfigPortalTimeout(180);
+                wifiManager.setAPCallback([this, &configPortalRunning](WiFiManager* wifiManager) {
+                    Serial.println("WiFi: entered config portal");
+                    configPortalRunning.setFromISR();
+                });
+                wifiManager.setConfigPortalTimeoutCallback([this, &configPortalRunning]() {
+                    Serial.println("WiFi: config portal timed out");
+                    configPortalRunning.clearFromISR();
+                });
                 bool connected = wifiManager.autoConnect(hostname.c_str());
                 xSemaphoreTake(reconnectSemaphor, connected ? portMAX_DELAY : 0);
                 Serial.println("WiFi: Reconnecting...");
@@ -72,7 +73,6 @@ private:
         portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
     }
 
-    WiFiManager wifiManager;
     QueueHandle_t reconnectSemaphor = xSemaphoreCreateBinary();
 };
 
