969dd7bf367df28f68d9d39eb16e08ea206f354d
Lóránt Pintér <lorant.pinter@gmail.com>
2023-12-16 20:36:31 +0100

    Move ConsolePrinter to Application

--- src/devices/Device.hpp
+++ src/devices/Device.hpp
@@ -11,110 +11,16 @@ using namespace farmhub::kernel::drivers;
 
 namespace farmhub { namespace devices {
 
-class ConsolePrinter {
-public:
-    ConsolePrinter() {
-        static const String spinner = "|/-\\";
-        Task::loop("ConsolePrinter", 8192, 1, [this](Task& task) {
-            Serial.print("\033[1G\033[0K");
-
-            counter = (counter + 1) % spinner.length();
-            Serial.print("[" + spinner.substring(counter, counter + 1) + "] ");
-
-            Serial.print("\033[33m" + String(VERSION) + "\033[0m");
-
-            Serial.print(", IP: \033[33m" + WiFi.localIP().toString() + "\033[0m");
-            Serial.print("/" + wifiStatus());
-
-            Serial.printf(", uptime: \033[33m%.1f\033[0m s", float(millis()) / 1000.0f);
-            time_t now;
-            struct tm timeinfo;
-            time(&now);
-            localtime_r(&now, &timeinfo);
-            Serial.print(&timeinfo, ", UTC: \033[33m%Y-%m-%d %H:%M:%S\033[0m");
-
-            Serial.printf(", heap: \033[33m%.2f\033[0m kB", float(ESP.getFreeHeap()) / 1024.0f);
-
-            BatteryDriver* battery = this->battery.load();
-            if (battery != nullptr) {
-                Serial.printf(", battery: \033[33m%.2f V\033[0m", battery->getVoltage());
-            }
-
-            Serial.print(" ");
-            Serial.flush();
-            task.delayUntil(milliseconds(100));
-        });
-    }
-
-    void registerBattery(BatteryDriver& battery) {
-        this->battery = &battery;
-    }
-
-private:
-    static String wifiStatus() {
-        switch (WiFi.status()) {
-            case WL_NO_SHIELD:
-                return "\033[0;31mno shield\033[0m";
-            case WL_IDLE_STATUS:
-                return "\033[0;33midle\033[0m";
-            case WL_NO_SSID_AVAIL:
-                return "\033[0;31mno SSID\033[0m";
-            case WL_SCAN_COMPLETED:
-                return "\033[0;33mscan completed\033[0m";
-            case WL_CONNECTED:
-                return "\033[0;32mOK\033[0m";
-            case WL_CONNECT_FAILED:
-                return "\033[0;31mfailed\033[0m";
-            case WL_CONNECTION_LOST:
-                return "\033[0;31mconnection lost\033[0m";
-            case WL_DISCONNECTED:
-                return "\033[0;33mdisconnected\033[0m";
-            default:
-                return "\033[0;31munknown\033[0m";
-        }
-    }
-
-    int counter;
-    std::atomic<BatteryDriver*> battery { nullptr };
-};
-
-class MemoryTelemetryProvider : public TelemetryProvider {
-public:
-    void populateTelemetry(JsonObject& json) override {
-        json["free-heap"] = ESP.getFreeHeap();
-    }
-};
-
-class ConsoleProvider {
-public:
-    ConsoleProvider() {
-        Serial.begin(115200);
-        Serial.println("Starting up...");
-    }
-};
-
 template <typename TDeviceConfiguration>
-class Device : ConsoleProvider {
+class Device {
 public:
     Device(gpio_num_t statusPin)
         : statusLed("status", statusPin) {
-#if defined(FARMHUB_DEBUG) || defined(FARMHUB_REPORT_MEMORY)
-        application.registerTelemetryProvider("memory", memoryTelemetryProvider);
-#endif
     }
 
     void begin() {
         application.begin();
     }
-
-protected:
-#ifdef FARMHUB_DEBUG
-    ConsolePrinter consolePrinter;
-#endif
-
-#if defined(FARMHUB_DEBUG) || defined(FARMHUB_REPORT_MEMORY)
-    MemoryTelemetryProvider memoryTelemetryProvider;
-#endif
 
 public:
     LedDriver statusLed;
@@ -129,7 +35,7 @@ public:
         : Device<TDeviceConfiguration>(statusPin)
         , batteryDriver(batteryPin, batteryVoltageDividerRatio) {
 #ifdef FARMHUB_DEBUG
-        Device<TDeviceConfiguration>::consolePrinter.registerBattery(batteryDriver);
+        Device<TDeviceConfiguration>::application.consolePrinter.registerBattery(batteryDriver);
 #endif
         Device<TDeviceConfiguration>::application.registerTelemetryProvider("battery", batteryDriver);
     }
--- src/kernel/Application.hpp
+++ src/kernel/Application.hpp
@@ -8,6 +8,7 @@
 #include <kernel/Command.hpp>
 #include <kernel/FileSystem.hpp>
 #include <kernel/Telemetry.hpp>
+#include <kernel/drivers/BatteryDriver.hpp>
 #include <kernel/drivers/LedDriver.hpp>
 #include <kernel/drivers/MdnsDriver.hpp>
 #include <kernel/drivers/MqttDriver.hpp>
@@ -25,6 +26,80 @@ template <typename TConfiguration>
 class Application;
 
 static RTC_DATA_ATTR int bootCount = 0;
+
+class ConsolePrinter {
+public:
+    ConsolePrinter() {
+        static const String spinner = "|/-\\";
+        Task::loop("ConsolePrinter", 8192, 1, [this](Task& task) {
+            Serial.print("\033[1G\033[0K");
+
+            counter = (counter + 1) % spinner.length();
+            Serial.print("[" + spinner.substring(counter, counter + 1) + "] ");
+
+            Serial.print("\033[33m" + String(VERSION) + "\033[0m");
+
+            Serial.print(", IP: \033[33m" + WiFi.localIP().toString() + "\033[0m");
+            Serial.print("/" + wifiStatus());
+
+            Serial.printf(", uptime: \033[33m%.1f\033[0m s", float(millis()) / 1000.0f);
+            time_t now;
+            struct tm timeinfo;
+            time(&now);
+            localtime_r(&now, &timeinfo);
+            Serial.print(&timeinfo, ", UTC: \033[33m%Y-%m-%d %H:%M:%S\033[0m");
+
+            Serial.printf(", heap: \033[33m%.2f\033[0m kB", float(ESP.getFreeHeap()) / 1024.0f);
+
+            BatteryDriver* battery = this->battery.load();
+            if (battery != nullptr) {
+                Serial.printf(", battery: \033[33m%.2f V\033[0m", battery->getVoltage());
+            }
+
+            Serial.print(" ");
+            Serial.flush();
+            task.delayUntil(milliseconds(100));
+        });
+    }
+
+    void registerBattery(BatteryDriver& battery) {
+        this->battery = &battery;
+    }
+
+private:
+    static String wifiStatus() {
+        switch (WiFi.status()) {
+            case WL_NO_SHIELD:
+                return "\033[0;31mno shield\033[0m";
+            case WL_IDLE_STATUS:
+                return "\033[0;33midle\033[0m";
+            case WL_NO_SSID_AVAIL:
+                return "\033[0;31mno SSID\033[0m";
+            case WL_SCAN_COMPLETED:
+                return "\033[0;33mscan completed\033[0m";
+            case WL_CONNECTED:
+                return "\033[0;32mOK\033[0m";
+            case WL_CONNECT_FAILED:
+                return "\033[0;31mfailed\033[0m";
+            case WL_CONNECTION_LOST:
+                return "\033[0;31mconnection lost\033[0m";
+            case WL_DISCONNECTED:
+                return "\033[0;33mdisconnected\033[0m";
+            default:
+                return "\033[0;31munknown\033[0m";
+        }
+    }
+
+    int counter;
+    std::atomic<BatteryDriver*> battery { nullptr };
+};
+
+class MemoryTelemetryProvider : public TelemetryProvider {
+public:
+    void populateTelemetry(JsonObject& json) override {
+        json["free-heap"] = ESP.getFreeHeap();
+    }
+};
 
 // TODO Move this to a separate file
 static const String& getMacAddress() {
@@ -73,8 +148,16 @@ public:
     }
 };
 
+class ConsoleProvider {
+public:
+    ConsoleProvider() {
+        Serial.begin(115200);
+        Serial.println("Starting up...");
+    }
+};
+
 template <typename TDeviceConfiguration>
-class Application {
+class Application : ConsoleProvider {
 public:
     Application(LedDriver& statusLed)
         : version(VERSION)
@@ -89,6 +172,10 @@ public:
             deviceConfig.getHostname());
 
         Task::loop("status-update", 4096, [this](Task&) { updateState(); });
+
+#if defined(FARMHUB_DEBUG) || defined(FARMHUB_REPORT_MEMORY)
+        registerTelemetryProvider("memory", memoryTelemetryProvider);
+#endif
 
         registerCommand(echoCommand);
         // TODO Add ping command
@@ -153,6 +240,10 @@ public:
             command.handle(request, response);
         });
     }
+
+#ifdef FARMHUB_DEBUG
+    ConsolePrinter consolePrinter;
+#endif
 
 private:
     enum class ApplicationState {
@@ -233,6 +324,10 @@ private:
 
     FileSystem& fs;
     TDeviceConfiguration deviceConfig;
+
+#if defined(FARMHUB_DEBUG) || defined(FARMHUB_REPORT_MEMORY)
+    MemoryTelemetryProvider memoryTelemetryProvider;
+#endif
 
     LedDriver& statusLed;
     ApplicationState state = ApplicationState::BOOTING;
