82ef8b896251def67675ccff570e599dcb2e4d96
Lóránt Pintér <lorant.pinter@gmail.com>
2023-12-21 08:22:29 +0100

    Mention that app has started up

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 
 #include <Arduino.h>
 
+#include <ArduinoLog.h>
+
 #include <devices/Device.hpp>
 
 extern "C" void app_main() {
     initArduino();
 
     new farmhub::devices::Device();
+
+    Log.infoln("Application initialized, entering idle loop");
 
     while (true) {
         vTaskDelay(portMAX_DELAY);
