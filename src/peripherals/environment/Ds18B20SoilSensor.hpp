3d955005845f2a927ac04c9bc0254c98e749de9a
Lóránt Pintér <lorant.pinter@gmail.com>
2024-02-28 17:04:59 +0100

    Let soil measurements be reported as environment

--- src/peripherals/environment/Ds18B20SoilSensor.hpp
+++ src/peripherals/environment/Ds18B20SoilSensor.hpp
@@ -2,12 +2,13 @@
 
 #include <memory>
 
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
 #include <devices/Peripheral.hpp>
 #include <kernel/Configuration.hpp>
 #include <kernel/drivers/MqttDriver.hpp>
 #include <peripherals/SinglePinDeviceConfig.hpp>
-
-#include "Ds18B20SoilSensorComponent.hpp"
 
 using namespace farmhub::devices;
 using namespace farmhub::kernel;
@@ -16,6 +17,73 @@ using namespace farmhub::peripherals;
 using std::make_unique;
 using std::unique_ptr;
 namespace farmhub::peripherals::environment {
+
+/**
+ * @brief Support for DS18B20 soil temperature sensor.
+ *
+ * Note: Needs a 4.7k pull-up resistor between the data and power lines.
+ */
+class Ds18B20SoilSensorComponent
+    : public Component,
+      public TelemetryProvider {
+public:
+    Ds18B20SoilSensorComponent(
+        const String& name,
+        shared_ptr<MqttDriver::MqttRoot> mqttRoot,
+        gpio_num_t pin)
+        : Component(name, mqttRoot) {
+
+        Log.infoln("Initializing DS18B20 soil temperature sensor on pin %d",
+            pin);
+
+        oneWire.begin(pin);
+
+        // locate devices on the bus
+        Log.verboseln("Locating devices...");
+        sensors.begin();
+        Log.traceln("Found %d devices, parasitic power is %s",
+            sensors.getDeviceCount(),
+            sensors.isParasitePowerMode() ? "ON" : "OFF");
+
+        DeviceAddress thermometer;
+        if (!sensors.getAddress(thermometer, 0)) {
+            throw PeripheralCreationException(name, "unable to find address for device");
+        }
+
+        // show the addresses we found on the bus
+        Log.traceln("Device 0 Address: %s",
+            toStringAddress(thermometer).c_str());
+    }
+
+    void populateTelemetry(JsonObject& json) override {
+        if (!sensors.requestTemperaturesByIndex(0)) {
+            Log.errorln("Failed to get temperature from DS18B20 sensor");
+            return;
+        }
+        float temperature = sensors.getTempCByIndex(0);
+        if (temperature == DEVICE_DISCONNECTED_C) {
+            Log.errorln("Failed to get temperature from DS18B20 sensor");
+            return;
+        }
+        json["temperature"] = temperature;
+    }
+
+private:
+
+    OneWire oneWire;
+
+    // Pass our oneWire reference to Dallas Temperature.
+    DallasTemperature sensors { &oneWire };
+
+    String toStringAddress(DeviceAddress address) {
+        char result[17];
+        for (int i = 0; i < 8; i++) {
+            sprintf(&result[i * 2], "%02X", address[i]);
+        }
+        result[16] = '\0';
+        return result;
+    }
+};
 
 class Ds18B20SoilSensor
     : public Peripheral<EmptyConfiguration> {
@@ -37,7 +105,7 @@ class Ds18B20SoilSensorFactory
     : public PeripheralFactory<SinglePinDeviceConfig, EmptyConfiguration> {
 public:
     Ds18B20SoilSensorFactory()
-        : PeripheralFactory<SinglePinDeviceConfig, EmptyConfiguration>("environment:ds18b20") {
+        : PeripheralFactory<SinglePinDeviceConfig, EmptyConfiguration>("environment:ds18b20", "environment") {
     }
 
     unique_ptr<Peripheral<EmptyConfiguration>> createPeripheral(const String& name, const SinglePinDeviceConfig& deviceConfig, shared_ptr<MqttDriver::MqttRoot> mqttRoot) override {
