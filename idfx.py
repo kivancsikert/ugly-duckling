a26f6a837e09f9688d352bc580629be1dce9e352
Lóránt Pintér <lorant.pinter@gmail.com>
2024-10-05 22:46:06 +0200

    Add debug option

--- idfx.py
+++ idfx.py
@@ -19,6 +19,7 @@ ud_gen = os.getenv("UD_GEN")
 if not ud_gen:
     print("Error: UD_GEN environment variable is not set.", file=sys.stderr)
     sys.exit(1)
+ud_debug = os.getenv("UD_DEBUG") == "1"
 
 idf_path = os.getenv("IDF_PATH")
 if not idf_path:
@@ -31,7 +32,7 @@ arduino_component_hash = r"managed_components/espressif__arduino-esp32/.componen
 if os.path.exists(arduino_component_hash):
     os.remove(arduino_component_hash)
 
-build_dir = f"build_{ud_gen.lower()}"
+build_dir = f"build/{ud_gen.lower()}-{'debug' if ud_debug else 'release'}"
 
 idf_py = f"{idf_path}/tools/idf.py"
 idf_args = sys.argv[1:]
@@ -49,9 +50,13 @@ idf_args = ud_args + idf_args
 env = {
     **os.environ,
     "IDF_TARGET": determine_idf_target(ud_gen),
+    "UD_GEN": ud_gen,
+    "UD_DEBUG": "1" if ud_debug else "0",
 }
 
-print(f"Running: {idf_py} {' '.join(idf_args)}")
+print(f"Running: {idf_py}")
+print(f"  Arguments: {idf_args}")
+print(f"  Build directory: {build_dir}")
 print(f"  Environment: IDF_TARGET={env['IDF_TARGET']}")
 try:
     result = subprocess.run([sys.executable, idf_py] + idf_args, check=True, env=env)
