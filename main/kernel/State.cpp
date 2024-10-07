4be396461ad21a4552c28796f69eb3b9733e0ced
Lóránt Pintér <lorant.pinter@gmail.com>
2024-10-08 01:14:11 +0200

    Remove redundant IRAM_ATTR

--- main/kernel/State.cpp
+++ main/kernel/State.cpp
@@ -6,7 +6,7 @@ bool StateSource::setFromISR() const {
     return hasAllBits(setBitsFromISR(eventBits | STATE_CHANGE_BIT_MASK));
 }
 
-bool IRAM_ATTR StateSource::clearFromISR() const {
+bool StateSource::clearFromISR() const {
     bool cleared = hasAllBits(xEventGroupClearBitsFromISR(eventGroup, eventBits));
     setBitsFromISR(STATE_CHANGE_BIT_MASK);
     return cleared;
