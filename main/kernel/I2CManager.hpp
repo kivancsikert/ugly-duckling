723c7f969b873068b26042333446ba9523d3f371
Lóránt Pintér <lorant.pinter@gmail.com>
2024-10-27 11:12:33 +0100

    Add support for I2C devices

--- main/kernel/I2CManager.hpp
+++ main/kernel/I2CManager.hpp
@@ -2,6 +2,7 @@
 
 #include <exception>
 #include <map>
+#include <memory>
 #include <utility>
 
 #include <Arduino.h>
@@ -11,6 +12,8 @@
 #include <kernel/Log.hpp>
 
 namespace farmhub::kernel {
+
+using std::shared_ptr;
 
 using farmhub::devices::PinPtr;
 
@@ -26,6 +29,94 @@ public:
     String toString() const {
         return String("I2C address: 0x") + String(address, HEX) + ", SDA: " + sda->getName() + ", SCL: " + scl->getName();
     }
+};
+
+class I2CTransmission;
+
+class I2CDevice {
+public:
+    I2CDevice(const String& name, TwoWire& wire, uint8_t address)
+        : name(name)
+        , wire(wire)
+        , address(address) {
+    }
+
+private:
+    const String name;
+    TwoWire& wire;
+    const uint8_t address;
+
+    friend class I2CTransmission;
+};
+
+class I2CTransmission {
+public:
+    I2CTransmission(shared_ptr<I2CDevice> device)
+        : device(device) {
+        device->wire.beginTransmission(device->address);
+    }
+
+    ~I2CTransmission() {
+        auto result = device->wire.endTransmission();
+        if (result != 0) {
+            Log.error("Communication unsuccessful with I2C device %s at address 0x%02x, result: %d",
+                device->name.c_str(), device->address, result);
+        }
+    }
+
+    size_t requestFrom(size_t len, bool stopBit = true) {
+        Log.debug("Requesting %d bytes from I2C device %s at address 0x%02x",
+            len, device->name.c_str(), device->address);
+        auto count = device->wire.requestFrom(device->address, len, stopBit);
+        Log.debug("Received %d bytes from I2C device %s at address 0x%02x",
+            count, device->name.c_str(), device->address);
+        return count;
+    }
+
+    size_t write(uint8_t data) {
+        Log.debug("Writing 0x%02x to I2C device %s at address 0x%02x",
+            data, device->name.c_str(), device->address);
+        auto count = device->wire.write(data);
+        Log.debug("Wrote %d bytes to I2C device %s at address 0x%02x",
+            count, device->name.c_str(), device->address);
+        return count;
+    }
+
+    size_t write(const uint8_t *data, size_t quantity) {
+        Log.debug("Writing %d bytes to I2C device %s at address 0x%02x",
+            quantity, device->name.c_str(), device->address);
+        auto count = device->wire.write(data, quantity);
+        Log.debug("Wrote %d bytes to I2C device %s at address 0x%02x",
+            count, device->name.c_str(), device->address);
+        return count;
+    }
+
+    int available() {
+        return device->wire.available();
+    }
+
+    int read() {
+        auto value = device->wire.read();
+        Log.debug("Read 0x%02x from I2C device %s at address 0x%02x",
+            value, device->name.c_str(), device->address);
+        return value;
+    }
+
+    int peek() {
+        auto value = device->wire.peek();
+        Log.debug("Peeked 0x%02x from I2C device %s at address 0x%02x",
+            value, device->name.c_str(), device->address);
+        return value;
+    }
+
+    void flush() {
+        Log.debug("Flushing I2C device %s at address 0x%02x",
+            device->name.c_str(), device->address);
+        device->wire.flush();
+    }
+
+private:
+    shared_ptr<I2CDevice> device;
 };
 
 class I2CManager {
@@ -55,6 +146,19 @@ public:
             wireMap[key] = wire;
             return *wire;
         }
+    }
+
+    shared_ptr<I2CDevice> createDevice(const String& name, const I2CConfig& config) {
+        return createDevice(name, config.sda, config.scl, config.address);
+    }
+
+    shared_ptr<I2CDevice> createDevice(const String& name, InternalPinPtr sda, InternalPinPtr scl, uint8_t address) {
+        auto device = std::make_shared<I2CDevice>(name, getWireFor(sda, scl), address);
+        Log.info("Created I2C device %s at address 0x%02x",
+            name.c_str(), address);
+        // Test if communication is possible
+        I2CTransmission tx(device);
+        return device;
     }
 
 private:
