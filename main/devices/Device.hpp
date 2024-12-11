a5ceb7fad2a18e67b0907353ab45f811a988f73f
Lóránt Pintér <lorant.pinter@gmail.com>
2024-12-11 16:30:49 +0100

    Polish debug console

--- main/devices/Device.hpp
+++ main/devices/Device.hpp
@@ -12,6 +12,7 @@
 
 #include <Print.h>
 
+#include <kernel/BootClock.hpp>
 #include <kernel/Command.hpp>
 #include <kernel/Concurrent.hpp>
 #include <kernel/Console.hpp>
@@ -111,6 +112,7 @@ private:
     void printStatus() {
         static const char* spinner = "|/-\\";
         static const int spinnerLength = strlen(spinner);
+        auto uptime = duration_cast<milliseconds>(boot_clock::now().time_since_epoch());
 
         status.clear();
         counter = (counter + 1) % spinnerLength;
@@ -123,13 +125,13 @@ private:
         status.concat("\033[0m");
 
         status.concat(", uptime: \033[33m");
-        status.concat(String(float(millis()) / 1000.0f, 1));
+        status.concat(String(uptime.count() / 1000.0, 1));
         status.concat("\033[0m s");
 
         status.concat(", WIFI: ");
         status.concat(wifiStatus());
         status.concat(" (up \033[33m");
-        status.concat(String(float(wifi.getUptime().count()) / 1000.0f, 1));
+        status.concat(String(double(wifi.getUptime().count()) / 1000.0, 1));
         status.concat("\033[0m s)");
 
         status.concat(", RTC: \033[33m");
@@ -137,7 +139,7 @@ private:
         status.concat("\033[0m");
 
         status.concat(", heap: \033[33m");
-        status.concat(String(float(ESP.getFreeHeap()) / 1024.0f, 2));
+        status.concat(String(double(heap_caps_get_free_size(MALLOC_CAP_INTERNAL)) / 1024.0, 2));
         status.concat("\033[0m kB");
 
         status.concat(", CPU: \033[33m");
