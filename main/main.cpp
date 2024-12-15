05ff615ae3dde6a92c56df12699b8e5326308f00
Lóránt Pintér <lorant.pinter@gmail.com>
2024-12-15 02:38:18 +0100

    Init NVS store early

--- main/main.cpp
+++ main/main.cpp
@@ -81,6 +81,16 @@ static void dumpPerTaskHeapInfo() {
 extern "C" void app_main() {
     Log::init();
 
+    // Initialize NVS
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // NVS partition was truncated and needs to be erased
+        // Retry nvs_flash_init
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
 #ifdef CONFIG_HEAP_TRACING
     ESP_ERROR_CHECK(heap_trace_init_standalone(trace_record, NUM_RECORDS));
 #endif
