24460706a84a7689cd84e63e437baf856a09a4a4
Lóránt Pintér <lorant.pinter@gmail.com>
2025-01-31 16:48:35 +0100

    Move crash manager out

--- main/devices/Device.hpp
+++ main/devices/Device.hpp
@@ -3,15 +3,14 @@
 #include <chrono>
 #include <memory>
 
-#include <esp_core_dump.h>
 #include <esp_netif.h>
 #include <esp_pm.h>
 #include <esp_wifi.h>
-#include <mbedtls/base64.h>
 
 #include <kernel/BootClock.hpp>
 #include <kernel/Command.hpp>
 #include <kernel/Concurrent.hpp>
+#include <kernel/CrashManager.hpp>
 #include <kernel/DebugConsole.hpp>
 #include <kernel/Strings.hpp>
 #include <kernel/Task.hpp>
@@ -48,6 +47,8 @@ typedef farmhub::devices::Mk8Config TDeviceConfiguration;
 #else
 #error "No device defined"
 #endif
+
+using namespace farmhub::kernel;
 
 namespace farmhub::devices {
 
@@ -166,7 +167,7 @@ public:
                 json["peripherals"].to<JsonArray>().set(peripheralsInitJson);
                 json["sleepWhenIdle"] = powerManager->sleepWhenIdle;
 
-                reportPreviousCrashIfAny(json);
+                CrashManager::reportPreviousCrashIfAny(json);
             },
             Retention::NoRetain, QoS::AtLeastOnce, 5s);
 
@@ -196,92 +197,6 @@ private:
         Success = 0,
         PeripheralError = 1,
     };
-
-    void reportPreviousCrashIfAny(JsonObject& json) {
-        if (!hasCoreDump()) {
-            return;
-        }
-
-        esp_core_dump_summary_t summary {};
-        esp_err_t err = esp_core_dump_get_summary(&summary);
-        if (err != ESP_OK) {
-            LOGE("Failed to get core dump summary: %s", esp_err_to_name(err));
-        } else {
-            auto crashJson = json["crash"].to<JsonObject>();
-            reportPreviousCrash(crashJson, summary);
-        }
-
-        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_core_dump_image_erase());
-    }
-
-    bool hasCoreDump() {
-        esp_err_t err = esp_core_dump_image_check();
-        switch (err) {
-            case ESP_OK:
-                return true;
-            case ESP_ERR_NOT_FOUND:
-                LOGV("No core dump found");
-                return false;
-            case ESP_ERR_INVALID_SIZE:
-                LOGD("Invalid core dump size");
-                // Typically, this happens when no core dump has been saved yet
-                return false;
-            default:
-                LOGE("Failed to check for core dump: %s", esp_err_to_name(err));
-                return false;
-        }
-    }
-
-    void reportPreviousCrash(JsonObject& json, const esp_core_dump_summary_t& summary) {
-        auto excCause =
-#if __XTENSA__
-            summary.ex_info.exc_cause;
-#else
-            summary.ex_info.mcause;
-#endif
-
-        LOGW("Core dump found: task: %s, cause: %ld",
-            summary.exc_task, excCause);
-
-        json["version"] = summary.core_dump_version;
-        json["sha256"] = std::string((const char*) summary.app_elf_sha256);
-        json["task"] = std::string(summary.exc_task);
-        json["cause"] = excCause;
-
-        static constexpr size_t PANIC_REASON_SIZE = 256;
-        char panicReason[PANIC_REASON_SIZE];
-        if (esp_core_dump_get_panic_reason(panicReason, PANIC_REASON_SIZE) == ESP_OK) {
-            LOGW("Panic reason: %s", panicReason);
-            json["panicReason"] = std::string(panicReason);
-        }
-
-#ifdef __XTENSA__
-        auto backtraceJson = json["backtrace"].to<JsonObject>();
-        if (summary.exc_bt_info.corrupted) {
-            LOGE("Backtrace corrupted, depth %lu", summary.exc_bt_info.depth);
-            backtraceJson["corrupted"] = true;
-        } else {
-            auto framesJson = backtraceJson["frames"].to<JsonArray>();
-            for (int i = 0; i < summary.exc_bt_info.depth; i++) {
-                auto& frame = summary.exc_bt_info.bt[i];
-                framesJson.add("0x" + toHexString(frame));
-            }
-        }
-#else
-        size_t encodedLen = (summary.exc_bt_info.dump_size + 2) / 3 * 4 + 1;
-        unsigned char encoded[encodedLen];
-
-        size_t writtenLen = 0;
-        int ret = mbedtls_base64_encode(encoded, sizeof(encoded), &writtenLen, summary.exc_bt_info.stackdump, summary.exc_bt_info.dump_size);
-
-        if (ret == 0) {
-            encoded[writtenLen] = '\0';    // Null-terminate the output string
-            json["stackdump"] = encoded;
-        } else {
-            LOGE("Failed to encode stackdump: %d", ret);
-        }
-#endif
-    }
 
     const std::shared_ptr<TDeviceDefinition> deviceDefinition;
     const std::shared_ptr<FileSystem> fs;
