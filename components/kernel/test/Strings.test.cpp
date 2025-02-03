687a3fb62c992be0421ba8d5eb8c8baf159fb575
Lóránt Pintér <lorant.pinter@gmail.com>
2025-02-03 16:35:49 +0100

    Use Catch2 instead of Unity

--- components/kernel/test/Strings.test.cpp
+++ components/kernel/test/Strings.test.cpp
@@ -1,10 +1,10 @@
-#include <unity.h>
+#include <catch2/catch_test_macros.hpp>
 
 #include <Strings.hpp>
 
-TEST_CASE("toHexString works", "[test]") {
-    TEST_ASSERT_EQUAL_STRING("0", farmhub::kernel::toHexString(0).c_str());
-    TEST_ASSERT_EQUAL_STRING("1", farmhub::kernel::toHexString(1).c_str());
-    TEST_ASSERT_EQUAL_STRING("f", farmhub::kernel::toHexString(15).c_str());
-    TEST_ASSERT_EQUAL_STRING("123456ab", farmhub::kernel::toHexString(0x123456ab).c_str());
+TEST_CASE("toHexString") {
+    REQUIRE(farmhub::kernel::toHexString(0) == "0");
+    REQUIRE(farmhub::kernel::toHexString(1) =="1");
+    REQUIRE(farmhub::kernel::toHexString(15) == "f");
+    REQUIRE(farmhub::kernel::toHexString(0x123456ab) == "123456ab");
 }
