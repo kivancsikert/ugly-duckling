46ac856c40a6a1d31780589abfa2b0c7b72cc22f
Lóránt Pintér <lorant.pinter@gmail.com>
2025-02-16 10:52:27 +0100

    Move MovingAverage test to Catch2

---
+++ components/kernel/test/MovingAverageTest.cpp
@@ -0,0 +1,41 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <MovingAverage.hpp>
+
+using namespace farmhub::kernel;
+
+TEST_CASE("empty instance returns zero") {
+    MovingAverage<double> ma(3);
+    REQUIRE(ma.getAverage() == 0);
+}
+
+TEST_CASE("single measurement is returned") {
+    MovingAverage<double> ma(3);
+    ma.record(1);
+    REQUIRE(ma.getAverage() == 1);
+}
+
+TEST_CASE("two measurements are averaged") {
+    MovingAverage<double> ma(3);
+    ma.record(1);
+    ma.record(2);
+    REQUIRE(ma.getAverage() == 1.5);
+}
+
+TEST_CASE("at capacity measurements are averaged") {
+    MovingAverage<double> ma(3);
+    ma.record(1);
+    ma.record(2);
+    ma.record(3);
+    REQUIRE(ma.getAverage() == 2);
+}
+
+TEST_CASE("over capacity measurements are discarded") {
+    MovingAverage<double> ma(3);
+    ma.record(1);
+    ma.record(2);
+    ma.record(3);
+    ma.record(4);
+    ma.record(5);
+    REQUIRE(ma.getAverage() == 4);
+}
