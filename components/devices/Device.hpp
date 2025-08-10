3593228221bcdf17366bee69a135437fd59d989b
Lóránt Pintér <lorant.pinter@gmail.com>
2025-08-10 17:19:59 +0200

    Correct log message

--- components/devices/Device.hpp
+++ components/devices/Device.hpp
@@ -179,7 +179,7 @@ void registerBasicCommands(const std::shared_ptr<MqttRoot>& mqttRoot) {
     mqttRoot->registerCommand("sleep", [](const JsonObject& request, JsonObject& /*response*/) {
         seconds duration = seconds(request["duration"].as<int64_t>());
         esp_sleep_enable_timer_wakeup((microseconds(duration)).count());
-        LOGI("Sleeping for %lld seconds in light sleep mode",
+        LOGI("Sleeping deep for %lld seconds",
             duration.count());
         esp_deep_sleep_start();
     });
