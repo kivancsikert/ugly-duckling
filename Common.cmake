c3745965f0937a90b4ba1b25fc1ff4bf5e4ea9d2
Lóránt Pintér <lorant.pinter@gmail.com>
2025-08-12 11:33:59 +0200

    Use esp-idf-lib components as ESP-IDF components

--- Common.cmake
+++ Common.cmake
@@ -1,14 +1,5 @@
 list(APPEND EXTRA_COMPONENT_DIRS "${CMAKE_CURRENT_LIST_DIR}/components")
 
-include(FetchContent)
-FetchContent_Declare(
-    esp_idf_lib
-    GIT_REPOSITORY https://github.com/esp-idf-lib/core.git
-    GIT_TAG "254acb13520ca8c13f261b3676a52cd8d352f2f8"
-)
-FetchContent_MakeAvailable(esp_idf_lib)
-list(APPEND EXTRA_COMPONENT_DIRS ${esp_idf_lib_SOURCE_DIR}/components)
-
 include($ENV{IDF_PATH}/tools/cmake/project.cmake)
 
 add_compile_options("-Wno-missing-field-initializers")
