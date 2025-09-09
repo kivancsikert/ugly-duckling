b5452c86278a180fd9bf3c6423038eb475e22dc8
Lóránt Pintér <lorant.pinter@gmail.com>
2025-09-09 17:36:40 +0200

    Allow specifying DS18B20 address directly

--- components/peripherals/peripherals/environment/Ds18B20SoilSensor.hpp
+++ components/peripherals/peripherals/environment/Ds18B20SoilSensor.hpp
@@ -9,7 +9,6 @@
 #include <Configuration.hpp>
 
 #include <peripherals/Peripheral.hpp>
-#include <peripherals/SinglePinSettings.hpp>
 #include <peripherals/api/ITemperatureSensor.hpp>
 
 using namespace farmhub::kernel;
@@ -17,6 +16,11 @@ using namespace farmhub::kernel::mqtt;
 using namespace farmhub::peripherals;
 
 namespace farmhub::peripherals::environment {
+
+struct Ds18B20Settings : ConfigurationSection {
+    Property<InternalPinPtr> pin { this, "pin" };
+    Property<std::string> address { this, "address" };
+};
 
 /**
  * @brief Support for DS18B20 soil temperature sensor.
@@ -29,7 +33,8 @@ class Ds18B20SoilSensor final
 public:
     explicit Ds18B20SoilSensor(
         const std::string& name,
-        const InternalPinPtr& pin)
+        const InternalPinPtr& pin,
+        const std::string& address)
         : Peripheral(name)
         , pin(pin) {
 
@@ -39,20 +44,26 @@ public:
         // We rely on the external resistor for pull-up
         gpio_set_pull_mode(pin->getGpio(), GPIO_FLOATING);
 
-        LOGV("Locating DS18B20 sensors on bus...");
-        size_t sensorCount;
-        // TODO How many slots do we need here actually?
-        int maxSensors = 1;
-
-        esp_err_t searchResult = ds18x20_scan_devices(pin->getGpio(), &sensor, maxSensors, &sensorCount);
-        if (searchResult == ESP_OK) {
-            if (sensorCount == 0U) {
-                throw PeripheralCreationException("No DS18B20 sensors found on bus");
-            }
-            LOGD("Found a DS18B20 at address: %016llX", sensor);
+        if (!address.empty()) {
+            uint64_t parsedAddress = std::strtoull(address.c_str(), nullptr, 16);
+            sensor = std::byteswap(parsedAddress);
         } else {
-            throw PeripheralCreationException("Error searching for DS18B20 devices: " + std::string(esp_err_to_name(searchResult)));
+            LOGV("Locating DS18B20 sensors on bus...");
+            size_t sensorCount;
+            // TODO How many slots do we need here actually?
+            int maxSensors = 1;
+
+            esp_err_t searchResult = ds18x20_scan_devices(pin->getGpio(), &sensor, maxSensors, &sensorCount);
+            if (searchResult == ESP_OK) {
+                if (sensorCount == 0U) {
+                    throw PeripheralCreationException("No DS18B20 sensors found on bus");
+                }
+            } else {
+                throw PeripheralCreationException("Error searching for DS18B20 devices: " + std::string(esp_err_to_name(searchResult)));
+            }
         }
+
+        LOGD("Using DS18B20 sensor at address: %016llX", sensor);
     }
 
     Celsius getTemperature() override {
@@ -87,13 +98,14 @@ private:
 };
 
 inline PeripheralFactory makeFactoryForDs18b20() {
-    return makePeripheralFactory<ITemperatureSensor, Ds18B20SoilSensor, SinglePinSettings>(
+    return makePeripheralFactory<ITemperatureSensor, Ds18B20SoilSensor, Ds18B20Settings>(
         "environment:ds18b20",
         "environment",
-        [](PeripheralInitParameters& params, const std::shared_ptr<SinglePinSettings>& settings) {
+        [](PeripheralInitParameters& params, const std::shared_ptr<Ds18B20Settings>& settings) {
             auto sensor = std::make_shared<Ds18B20SoilSensor>(
                 params.name,
-                settings->pin.get());
+                settings->pin.get(),
+                settings->address.get());
             params.registerFeature("temperature", [sensor](JsonObject& telemetryJson) {
                 telemetryJson["value"] = sensor->getTemperature();
             });
