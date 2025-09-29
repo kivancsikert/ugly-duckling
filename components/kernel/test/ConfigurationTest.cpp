a200fb3b95a7d25778ff9e498d38aef7561d6e4e
Lóránt Pintér <lorant.pinter@gmail.com>
2025-09-29 20:22:10 +0200

    Add nested tests

--- components/kernel/test/ConfigurationTest.cpp
+++ components/kernel/test/ConfigurationTest.cpp
@@ -6,7 +6,7 @@
 
 using namespace farmhub::kernel;
 
-struct NestedConfig : ConfigurationSection {
+struct TestNestedConfig : ConfigurationSection {
     Property<int> intValue { this, "intValue" };
 };
 
@@ -14,7 +14,7 @@ struct TestConfig : ConfigurationSection {
     Property<int> intValue { this, "intValue" };
     Property<std::string> stringValue { this, "stringValue" };
     Property<bool> boolValue { this, "boolValue" };
-    NamedConfigurationEntry<NestedConfig> nested { this, "nested" };
+    NamedConfigurationEntry<TestNestedConfig> nested { this, "nested" };
 };
 
 std::string toString(const ConfigurationEntry& config) {
@@ -69,15 +69,19 @@ TEST_CASE("configuration with values is loaded from JSON and is stored as JSON")
     REQUIRE(toString(config) == R"({"intValue":42,"stringValue":"hello","boolValue":true,"nested":{"intValue":7}})");
 }
 
-struct ConfigWithDefaults : public ConfigurationSection {
+struct TestNestedConfigWithDefaults : ConfigurationSection {
+    Property<int> intValue { this, "intValue", 100 };
+};
+
+struct TestConfigWithDefaults : public ConfigurationSection {
     Property<int> intValue { this, "intValue", 42 };
     Property<std::string> stringValue { this, "stringValue", "default" };
     Property<bool> boolValue { this, "boolValue", true };
-    // TODO Add some nested configuration with defaults
+    NamedConfigurationEntry<TestNestedConfigWithDefaults> nested { this, "nested" };
 };
 
 TEST_CASE("configuration with default values loaded from empty JSON has default values") {
-    ConfigWithDefaults config;
+    TestConfigWithDefaults config;
     REQUIRE(toString(config) == R"({})");
     REQUIRE(!config.intValue.hasValue());
     REQUIRE(config.intValue.get() == 42);
@@ -85,18 +89,24 @@ TEST_CASE("configuration with default values loaded from empty JSON has default 
     REQUIRE(config.stringValue.get() == "default");
     REQUIRE(!config.boolValue.hasValue());
     REQUIRE(config.boolValue.get() == true);
+    REQUIRE(!config.nested.hasValue());
+    REQUIRE(!config.nested.get()->intValue.hasValue());
+    REQUIRE(config.nested.get()->intValue.get() == 100);
 }
 
 TEST_CASE("configuration with default values loaded from non-empty JSON has actual values") {
-    ConfigWithDefaults config;
-    config.loadFromString(R"({"intValue": 100, "stringValue": "custom", "boolValue": false})");
-    REQUIRE(toString(config) == R"({"intValue":100,"stringValue":"custom","boolValue":false})");
+    TestConfigWithDefaults config;
+    config.loadFromString(R"({"intValue": 100, "stringValue": "custom", "boolValue": false, "nested": {"intValue": 200}})");
+    REQUIRE(toString(config) == R"({"intValue":100,"stringValue":"custom","boolValue":false,"nested":{"intValue":200}})");
     REQUIRE(config.intValue.hasValue());
     REQUIRE(config.intValue.get() == 100);
     REQUIRE(config.stringValue.hasValue());
     REQUIRE(config.stringValue.get() == "custom");
     REQUIRE(config.boolValue.hasValue());
     REQUIRE(config.boolValue.get() == false);
+    REQUIRE(config.nested.hasValue());
+    REQUIRE(config.nested.get()->intValue.hasValue());
+    REQUIRE(config.nested.get()->intValue.get() == 200);
 }
 
 // TODO Add some tests for invalid JSON handling
